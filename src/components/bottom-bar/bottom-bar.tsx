59d1ef7f99b5d4ac261d4e4d06aef86f9f404d92
Elio <netoo.elio@hotmail.com>
2023-04-05 21:36:45 -0300

    redirect function

--- src/components/bottom-bar/bottom-bar.tsx
+++ src/components/bottom-bar/bottom-bar.tsx
@@ -1,19 +1,26 @@
 import { FaComment, FaDog, FaHome } from "react-icons/fa";
 import "./bottom-bar.css";
+import { redirect } from "../../utils/utils";
 export function BottomBar() {
   return (
     <div className="bar-container">
-      <div className="item-menu">
+      <div
+        className="item-menu"
+        onClick={() => {
+          redirect("/home");
+        }}
+      >
         <FaHome className="icon-menu" />
-        <span className="active">Inicio</span>
+        <span className="active">Início</span>
       </div>
-      <div className="item-menu">
+      <div
+        className="item-menu"
+        onClick={() => {
+          redirect("/chat");
+        }}
+      >
         <FaComment className="icon-menu" />
-        <span>Inicio</span>
-      </div>
-      <div className="item-menu">
-        <FaDog className="icon-menu" />
-        <span>Inicio</span>
+        <span>Conversas</span>
       </div>
     </div>
   );
