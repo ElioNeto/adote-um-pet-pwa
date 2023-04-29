c98e846855d2f215cfd50ce015db281fb36ddb1b
Esdras <esdraspmartins@hotmail.com>
2023-04-28 21:14:25 -0300

    Update registerpet.tsx

--- src/pages/registerpet/registerpet.tsx
+++ src/pages/registerpet/registerpet.tsx
@@ -10,47 +10,96 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import Stack from '@mui/material/Stack';
-
-const homeLogin = () => {
-  window.location.href = "/home";
-};
+import { useState } from "react";
 
 export function Registerpet() {
+
+  const homeLogin = () => {
+    window.location.href = "/home";
+  };
+  
+  const [formData, setFormData] = useState({
+    petName: "",
+    petRace: "",
+    petLocation: "",
+    petColor: "",
+    petWeight: "",
+    petAge: "",
+    petInfo: "",
+    petOptions: "",
+    petSexOptions: "",
+  });
+  
+  const[erros, setErrors] = useState('');
+  
+  function handleChange(e: any) {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+  
+  function handleValidation(e:any) {
+    e.preventDefault();
+    let formIsValid = true;
+    setErrors("");
+  
+    if(typeof formData.petAge !== "undefined") {
+      if(!formData.petAge.match(/^[0-9]+$/)) {
+        formIsValid = false;
+        setErrors("No campo Idade somente números é permitido");
+      }
+    }
+  
+    if(typeof formData.petWeight !== "undefined") {
+      if(!formData.petWeight.match(/^[0-9]+$/)) {
+        formIsValid = false;
+        setErrors("No campo Peso somente números é permitido");
+      }
+    }
+
+    if(formIsValid) {
+      homeLogin();
+    }
+  
+  }
+
   return (
     <div className="container">
       <div className="logo">
         <img src={Logo} alt="logo" />
       </div>
-      <div className="form">
+      <form className="form" onSubmit={(e) => handleValidation(e)}>
         <FormControl>
           <RadioGroup aria-labelledby="demo-radio-buttons-group-label"
                       defaultValue="dog"
-                      name="radio-buttons-group">
+                      name="petOptions"
+                      onChange={(e) => handleChange(e)}>
               <FormControlLabel value="dog" control={<Radio />} label="Cachorro" />
               <FormControlLabel value="cat" control={<Radio />} label="Gato" />
               <FormControlLabel value="othersPets" control={<Radio />} label="Outros pets" />
           </RadioGroup>
         </FormControl>
-          <input type="text" placeholder="Nome" name="petName"/>
-          <input type="text" placeholder="Raça" name="petRace"/>
-          <input type="text" placeholder="Localização" name="petLocation"/>
-          <input type="text" placeholder="Cor" name="petColor"/>
-          <input type="text" placeholder="Peso" name="petWeight"/>
-          <input type="text" placeholder="Idade" name="petAge"/>
-          <input type="text" placeholder="Fale algo sobre o PET" name="petInfo"/>
+          <input type="text" placeholder="Nome" name="petName" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Raça" name="petRace" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Localização" name="petLocation" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Cor" name="petColor" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Peso" name="petWeight" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Idade" name="petAge" onChange={(e) => handleChange(e)} required/>
+          <input type="text" placeholder="Fale algo sobre o PET" name="petInfo" onChange={(e) => handleChange(e)} />
         
         <FormControl>
           <FormLabel id="demo-radio-buttons-group-label">Sexo</FormLabel>
           <RadioGroup aria-labelledby="demo-radio-buttons-group-label"
                       defaultValue="female"
-                      name="radio-buttons-group">
+                      name="petSexOptions"
+                      onChange={(e) => handleChange(e)}>
               <FormControlLabel value="female" control={<Radio />} label="F" />
               <FormControlLabel value="male" control={<Radio />} label="M" />
           </RadioGroup>
         </FormControl>
         <input type="file" placeholder="Foto do Pet" name="petPic"/>
-        <button onClick={homeLogin}>Cadastrar o PET</button>
-      </div>
+        <span>{erros}</span>
+        <button >Cadastrar o PET</button>
+      </form>
     </div>
   );
 }
