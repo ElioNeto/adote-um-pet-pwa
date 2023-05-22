bba928a00f5f073ae64a5c5bc69b1d09002574c2
Esdras <esdraspmartins@hotmail.com>
2023-05-22 20:33:32 -0300

    Update favorites.tsx

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -2,14 +2,48 @@ import Logo from "../../assets/AdoteUmPet.svg";
 import "./favorites.css";
 import { Card } from "../../components/card/card";
 import { BottomBar } from "../../components/bottom-bar/bottom-bar";
-import { authValidate, getFromLocalStorage, redirect } from "../../utils/utils";
-import { useEffect } from "react";
-import { LIKE_COLLECTION, USER } from "../../utils/constants";
+import { authValidate, getFromLocalStorage, getSessionItem, redirect } from "../../utils/utils";
+import { useEffect, useState } from "react";
+import { readSubsData } from "../../utils/firebase";
+import { LIKE_COLLECTION, PETS_COLLECTION, USER } from "../../utils/constants";
 
 export function Favorites() {
+  
+  const [petData, setPetData] = useState<any[]>([]);
+  let user = "";
+
   useEffect(() => {
     if (!authValidate()) redirect("/");
-  });
+    else user = getSessionItem(USER)!;
+  }, []);
+
+  useEffect(() => {
+    readSubsData(LIKE_COLLECTION, (cb: any) => {
+      let arr: any[] = [];
+      Object.keys(cb).forEach((key) => {
+        if (cb[key].user === user) {
+          console.log(key); //column01...
+          console.log(cb[key]); //Coluna 01...
+          let obj = {
+            data: cb[key],
+            id: key,
+          };
+          arr.push(obj);
+        }
+      });
+      //like tem o pet (id) e acha petData.pet 
+      setPetData(arr);
+      console.log(arr);
+      readSubsData(PETS_COLLECTION, (pet: any) => {
+        let arrPet: any[] = [];
+        console.log(pet);
+       // petData.map((cbPet: any) => {
+          
+          //if (cbPet.data.pet === pet[])
+       // });
+      });
+    });
+  }, []);
 
   function getFav() {
     getFromLocalStorage(USER);
@@ -26,6 +60,11 @@ export function Favorites() {
           <div className="cards-fav">
             {/*  <Card />
             <Card /> */}
+          </div>
+          <div className="see-more-card-container">
+            {petData.map((pet: any) => (
+              <Card data={pet} />
+            ))}
           </div>
         </div>
       </div>
