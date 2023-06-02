099db66d1989830dc162a4f50661d6be6a8348ac
Esdras <esdraspmartins@hotmail.com>
2023-06-01 22:43:38 -0300

    header popup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import { FaArrowLeft, FaHeart } from "react-icons/fa";
 
 import "./header.css";
 import { like } from "../../utils/utils";
+import { useState } from "react";
 
 export function Header(props: {
   location: string;
@@ -9,12 +10,21 @@ export function Header(props: {
   user: any;
   pet: any;
 }) {
+  const [showPopup, setShowPopup] = useState(false);
+
   function backButton(location: string) {
     console.log("back", location);
     window.location.href = location;
   }
   function likeButton() {
     like(props.user, props.pet);
+    setShowPopup(true);
+    const timer = setTimeout(() => {
+      hidePopup();
+    }, 2000);
+  }
+  function hidePopup() {
+    setShowPopup(false);
   }
   return (
     <div className="header-container">
@@ -26,6 +36,12 @@ export function Header(props: {
           <FaHeart className="header-icon" />
         </div>
       )}
+      {showPopup && (
+        <div className="popup-container">
+          <p className="popup-message">Favoritado com sucesso!</p>
+        </div>
+      )}
     </div>
   );
+  
 }
